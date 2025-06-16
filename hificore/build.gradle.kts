7ec9ebb643c83555203d070c5a43598255735857
ghhccghk <2137610394@qq.com>
2025-06-16 22:24:02 +0800

    fix:编译bug

--- hificore/build.gradle.kts
+++ hificore/build.gradle.kts
@@ -55,5 +55,4 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation("io.github.nift4.dlfunc:dlfunc:0.1.6")
-    coreLibraryDesugaring('com.android.tools:desugar_jdk_libs:2.1.5')
 }
