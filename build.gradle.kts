73fb8ab835d1dc945b404367a15a9763d17749be
ghhccghk <2137610394@qq.com>
2025-06-17 09:15:41 +0800

    ci:取消去除ci缓存

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,18 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.abc) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("androidx.baselineprofile") version "1.3.4" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "12.2.1" apply false
+    id("com.osacky.doctor") version "0.11.0"
+}
+
+doctor {
+    javaHome {
+        ensureJavaHomeMatches.set(false)
+        ensureJavaHomeIsSet.set(false)
+    }
+}
+
+tasks.withType(JavaCompile::class.java) {
+    options.compilerArgs.add("-Xlint:all")
 }
