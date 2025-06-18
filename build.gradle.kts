ce53a0fc6c0a44fe2efc405bfa78db96c5d25ea0
ghhccghk <2137610394@qq.com>
2025-06-18 13:26:54 +0800

    fix:去除分包机制，防止aab编译错误

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.abc) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     id("androidx.baselineprofile") version "1.3.4" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "12.2.1" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "12.2.3" apply false
     id("com.osacky.doctor") version "0.11.0"
 }
 
