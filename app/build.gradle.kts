5c27f5cd6200d44bad402d481f26d737e6397d7d
ghhccghk <2137610394@qq.com>
2025-08-04 18:34:17 +0800

    取消在发布版本中使用调试签名配置。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,7 +143,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
+//            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
